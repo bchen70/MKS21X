028ecd2e54fe76b048876c6d57f4515c9a1f02a8
bchen70 <bchen17@stuy.edu>
2017-11-28 14:22:33 -0500

    Book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,12 @@
+public class Book{
+    private string author;
+    private string title;
+    private string isbn;
+
+    public Book(String writer,String bookti, String intSBN){
+	author = writer;
+	title = bookti;
+	isbn = intSBN;
+    }
+    
+}
