56695d080d89105784775aa3bf8b5909e16b8f27
bchen70 <bchen17@stuy.edu>
2017-12-06 22:41:59 -0500

    Added toCode, changed getCode

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -3,14 +3,35 @@ public class Barcode{
 
   public Barcode(String zip){
     if (zip.length() != 5){
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException("Zip isn't correct length");
     }
-    for (int x = 0; x < zip.length(); x++) {
-      if((int)zip.charAt(x) - '0' > 0 || (int)zip.charAt(x) - '0' > 9) {
-        throw new IllegalArgumentException();
+    for (int i = 0; i < zip.length(); i++) {
+      if((int)zip.charAt(i) - '0' > 0 || (int)zip.charAt(i) - '0' > 9) {
+        throw new IllegalArgumentException("Zip has a non digit");
       }
     }
     this.zip = zip;
+  }
+
+  public static String toCode(String zip){
+    if (zip.length()!=5){
+      throw new IllegalArgumentException("Zip isn't correct length");
+    }
+    for (int i = 0; i < zip.length(); i++){
+      if ((int)zip.charAt(i) - '0' > 0 || (int)zip.charAt(i) - '0' > 9){
+        throw new IllegalArgumentException("Zip has non digit");
+      }
+    }
+    String[] a = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    String answer = "|";
+    int check = 0;
+    for (int i = 0; i < zip.length();i++){
+      int digit = Integer.valueOf(zip.charAt(i)+"");
+      check += digit;
+      answer+=a[digit];
+    }
+    answer += a[check%10] + "|";
+    return answer;
   }
 
   public String getZip(){
@@ -18,18 +39,12 @@ public class Barcode{
   }
 
   public String getCode() {
-    String[] a = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
-    String answer  = "|";
-    int check = 0;
-    for (int x = 0; x < zip.length(); x++) {
-      answer += a[(int)zip.charAt(x) - '0'];
-      check += (int)zip.charAt(x)- '0';
-    }
-    return answer + a[check % 10] + "|";
-    }
+    return toCode(zip);
+  }
 
-    public String toString() {
-      return getCode() + "(" + zip + ")";
+
+  public String toString() {
+    return getCode() + "(" + zip + ")";
     }
 
   public int compareTo(Barcode b) {
