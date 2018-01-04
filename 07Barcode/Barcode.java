477e17ef34f35144e4decf242feb3c18745270e9
bchen70 <bchen17@stuy.edu>
2018-01-04 16:35:11 -0500

    Fixed 07Barcode and tested

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,14 +1,16 @@
-public class Barcode{
+import java.util.*;
+public class Barcode implements Comparable<Barcode>{
   private String zip;
 
   public Barcode(String zip){
     if (zip.length() != 5){
       throw new IllegalArgumentException("Zip isn't correct length");
     }
-    for (int i = 0; i < zip.length(); i++) {
-      if((int)zip.charAt(i) - '0' > 0 || (int)zip.charAt(i) - '0' > 9) {
-        throw new IllegalArgumentException("Zip has a non digit");
-      }
+    try {
+      int s;
+      s = Integer.parseInt(zip);
+    } catch (NumberFormatException e){
+      throw new IllegalArgumentException();
     }
     this.zip = zip;
   }
@@ -18,7 +20,7 @@ public class Barcode{
       throw new IllegalArgumentException("Zip isn't correct length");
     }
     for (int i = 0; i < zip.length(); i++){
-      if ((int)zip.charAt(i) - '0' > 0 || (int)zip.charAt(i) - '0' > 9){
+      if (!Character.isDigit(zip.charAt(i))){
         throw new IllegalArgumentException("Zip has non digit");
       }
     }
@@ -77,10 +79,46 @@ public class Barcode{
     }
 
   public int compareTo(Barcode b) {
-    return getZip().compareTo(b.getZip());
+    return this.getZip().compareTo(b.getZip());
     }
 
   public boolean equals(Barcode b) {
-      return getZip().equals(b.getZip());
+    return compareTo(b)== 0;
   }
+
+  public static void main(String[]args){
+
+    Barcode a = new Barcode("10282");
+    Barcode b = new Barcode("10282");
+    Barcode c = new Barcode("10101");
+
+    // System.out.println("a: " + a); //a: |:::||||:::::|:||::|:::|:|::||:| (10282)
+    // System.out.println("b: " + b); //b: |:::||||:::::|:||::|:::|:|::||:| (10282)
+    // System.out.println("c: " + c); //c: |:::||||::::::||||::::::||::||:| (10101)
+
+      // System.out.println(a.compareTo(b)); //0
+      //System.out.println(c.compareTo(a)); //negative
+      // System.out.println(a.compareTo(c)); //positive
+
+
+     // System.out.println(a.equals(b)); //true
+     // System.out.println(a.equals(c)); //false
+
+
+    // System.out.println(toCode("10282")); // returns |:::||||:::::|:||::|:::|:|::||:|
+    // System.out.println(toZip("|:::||||:::::|:||::|:::|:|::||:|")); //returns 10282
+
+
+      // System.out.println(toCode("1234")); //length less than 5
+      // System.out.println(toCode("123456")); // length greater than 5
+      //System.out.println(toCode("1234z")); //contains non digit
+
+      // Barcode x = new Barcode("123"); //length less than 5
+    // Barcode x = new Barcode("123456"); //length greater than 5
+    //Barcode x = new Barcode("123ab"); //contains non digit
+
+    // System.out.println(toZip("|||:::||:::||:::||:::||:::||:::")); //length less than 32
+    //System.out.println(toZip("|||:::||:::||:::||:::||:::||:::||||"));//length greater than 32
+
+    }
 }
